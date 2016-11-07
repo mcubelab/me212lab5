c93dfb73db3c2b5522416ff562a671132fdde7f6
Peter KT Yu <peterkty@gmail.com>
2016-11-07 09:28:18 -0500

    update to ROS Kinetic

--- software/config/environment.sh
+++ software/config/environment.sh
@@ -37,9 +37,8 @@ set_ros()
   if [ -f $CODE_BASE/catkin_ws/devel/setup.bash ]; then
     source $CODE_BASE/catkin_ws/devel/setup.bash
   else
-    source /opt/ros/indigo/setup.bash
+    source /opt/ros/kinetic/setup.bash
   fi
-  export ROS_PACKAGE_PATH=$HOME/pokebot/ros_ws/:$ROS_PACKAGE_PATH
 }
 
 # some useful commands
